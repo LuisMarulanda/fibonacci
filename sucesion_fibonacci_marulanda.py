ffa36ab69e8aff67c1db3ff2984351a5c4dd2e16
LuisMarulanda <49348314+LuisMarulanda@users.noreply.github.com>
2024-01-06 11:26:02 -0500

    Sucesion fibonacci
    hace sucesion fibonacci

---
+++ sucesion_fibonacci_marulanda.py
@@ -0,0 +1,18 @@
+def fibonacciSuma(numero):
+    fib_secuencia=[0,1]
+    while len(fib_secuencia)<numero:
+        fib_secuencia.append(fib_secuencia[-1]+fib_secuencia[-2])
+        return fib_secuencia[:numero]
+    
+
+
+numero_input = int(input("Por favor ingrese el numero input para ejecutar sucesion de fibonacci : "))
+
+if  numero_input <= 0:
+    print("Por favor ingrese un numero mayor a cero(0)")
+else:
+    resultado = fibonacciSuma(numero_input)
+    print("La suma de los numeros:")
+    print(numero_input)
+    print("El output/resultado:")
+    print(resultado)
